af014537d9e70a5fbd34f6bc49633e10dd931749
sheabryarly <52511707+sheabryarly@users.noreply.github.com>
2020-02-17 14:08:01 -0700

    Fixes Query REsults output

--- scripts/ActiveDirectory/Update_Managed_User_CWContacts.ps1
+++ scripts/ActiveDirectory/Update_Managed_User_CWContacts.ps1
@@ -86,7 +86,7 @@ Try {
 }
 
 If ($QueryResults.Count) {
-    Write-Host "$QueryResults.Contact_ExternalID.Count contact(s) found in query." 
+    Write-Host "$($QueryResults.Contact_ExternalID.Count) contact(s) found in query." 
 } Else {
     Write-Host "Query found no contacts to update. Exiting script."
     Exit  
