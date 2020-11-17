4b9c11fa4d3897c38bec371daeaf4242cc8d6659
sheabryarly <52511707+sheabryarly@users.noreply.github.com>
2020-11-16 17:41:46 -0700

    Create swlog.ps1

---
+++ lib/swlog.ps1
@@ -0,0 +1,23 @@
+﻿<#=============================================================
+Log-Message()
+
+=============================================================#>
+
+Function Log-Message () {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+            [String]$msg,
+        [Parameter(Mandatory=$true)]
+            [String]$filepath,
+        [Parameter(Mandatory=$false)]
+            [Switch]$quiet
+    )
+
+    $dateTime = (Get-Date)
+
+    "$(Get-Date $dateTime -Format G): " + $msg>>$filepath
+    If (-not ($quiet.IsPresent)) {
+        Write-Host $msg
+    }
+}
