55603edaf35ae7e422574b747adebad9b42340dd
sheabryarly <52511707+sheabryarly@users.noreply.github.com>
2021-01-25 12:02:36 -0700

    Update Update_Per_Device_Agreement_Additions.ps1

--- scripts/Billing/Update_Per_Device_Agreement_Additions.ps1
+++ scripts/Billing/Update_Per_Device_Agreement_Additions.ps1
@@ -89,7 +89,7 @@ If (Get-Module -ListAvailable -Name SimplySQL) {
 
 $Auth = Get-CWMAuth -PublicKey $PublicKey -PrivateKey $PrivateKey -ClientID $ClientID -ErrorAction Stop
 $DBPW = ConvertTo-SecureString 'D1PN3BI7BaAB6DPm' -AsPlainText -Force
-$Credentials = New-Object System.Management.Automation.PSCredential (“mysqladmin@localhost”, $DBPW)
+$Credentials = New-Object System.Management.Automation.PSCredential (“mysqladmin”, $DBPW)
 $SomethingChanged = 0
 
 $AutomateCountQuery = 
